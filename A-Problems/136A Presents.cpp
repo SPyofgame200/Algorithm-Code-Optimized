53e07006eec27041d8296c17abac13682b94888e
Hoang Anh Vo <52689671+spyofgame@users.noreply.github.com>
2019-09-29 09:35:02 +0700

    Create 136A Presents.cpp

---
+++ A-Problems/136A Presents.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    int n,x;
+    cin >> n;
+    int arr[n+1];
+    for (int i = 1; i <= n; ++i) {
+        cin >> x;
+        arr[x]=i;
+    }
+    for (int i = 1; i <= n; ++i) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
