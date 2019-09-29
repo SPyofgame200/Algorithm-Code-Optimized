9755c2207f1a5aed1662e865f6e91d6274154e89
Hoang Anh Vo <52689671+spyofgame@users.noreply.github.com>
2019-09-29 09:39:28 +0700

    Create 677A Vanya and Fence.cpp

---
+++ A-Problems/677A Vanya and Fence.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    int n, h, x, ans = 0;
+    cin >> n >> h;
+    
+    for (int i = 0; i < n; ++i) {
+        cin >> x;
+        ans += (x > h) ? 2 : 1;
+    }
+    
+    cout << ans;
+    return 0;
+}
