de7b55d50b2e2533d060ff922a35a832ce3f362d
Hoang Anh Vo <52689671+spyofgame@users.noreply.github.com>
2019-09-29 09:32:01 +0700

    Create 266B Queue at the School.cpp

---
+++ B-Problems/266B Queue at the School.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    int n, q;
+    string s;
+    cin >> n >> q >> s;
+ 
+    while (q--) {
+        for (int i = 1; i < s.size(); ++i) {
+            if (s[i-1]=='B' && s[i]=='G') {
+                swap(s[i-1], s[i]);
+                ++i;
+            }
+        }
+    }
+    cout << s;
+    return 0;
+}
