6a39abb9242bad04b909b70e16e30815ef6d4b80
Hoang Anh Vo <52689671+spyofgame@users.noreply.github.com>
2019-09-29 09:48:54 +0700

    Create 339B Xenia and Ringroad.cpp

---
+++ B-Problems/339B Xenia and Ringroad.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    int n, m;
+    cin >> n >> m;
+ 
+    int pred, x;
+    cin >> pred;
+    long long len = pred-1;
+    while (--m) {
+        cin >> x;
+        len+=x-pred;
+        if (x < pred) len+=n;
+        pred = x;
+    }
+ 
+    cout << len;
+    return 0;
+}
