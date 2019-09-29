ce7c9657213a611226c5a50ac9958ac7cbef2fcc
Hoang Anh Vo <52689671+spyofgame@users.noreply.github.com>
2019-09-29 18:22:54 +0700

    Update 118A - String Task.cpp

--- A-Problems/118A - String Task.cpp
+++ A-Problems/118A - String Task.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
- 
+
 using namespace std;
- 
+
+char c[] = {'a','o','y','e','u','i','A','O','Y','E','U','I'};
+
 int main() {
     string s;
-    char c[] = {'a','o','y','e','u','i','A','O','Y','E','U','I'};
     cin >> s;
-    
+
+    /// delete all vowels
     for (int i = 0; i < 12; ++i) {
         s.erase(remove(s.begin(),s.end(),c[i]),s.end());
     }
-    for (int i = 0; i < s.length(); ++i) {
-        if ((s[i]>64)&&(s[i]<91)) s[i]+=32;
-        cout << "." << s[i];
+
+    /// add '.' before consonant
+    for (int i = 0, len = s.size(); i < len; ++i) {
+        cout << "." << (s[i] < 'a' ? char(s[i] + 32) : s[i]);
     }
     return 0;
-} 
+}
