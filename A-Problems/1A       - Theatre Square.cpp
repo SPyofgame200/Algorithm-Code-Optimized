940874794ec4910cbdafa283887c58d7ce4d5eb2
Hoang Anh Vo <52689671+spyofgame@users.noreply.github.com>
2019-09-29 18:37:38 +0700

    Update 1A       - Theatre Square.cpp

--- A-Problems/1A       - Theatre Square.cpp
+++ A-Problems/1A       - Theatre Square.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <cmath>
-
+ 
 using namespace std;
-
+ 
 int main() {
-    int n, m, a;
+    long long n, m, a;
     cin >> n >> m >> a;
-    cout << (long long)(ceil(1.0 * n / a) * ceil(1.0 * m / a));
+    cout << ((n + a - 1) / a) * ((m + a - 1) / a);
     return 0;
 }
