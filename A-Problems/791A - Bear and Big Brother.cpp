75ad981a42ca3c2b4508aa5d16cf774d8891a0ab
Hoang Anh Vo <52689671+spyofgame@users.noreply.github.com>
2019-10-26 10:17:59 +0700

    Update 791A - Bear and Big Brother.cpp

--- A-Problems/791A - Bear and Big Brother.cpp
+++ A-Problems/791A - Bear and Big Brother.cpp
@@ -3,11 +3,15 @@
 using namespace std;
  
 int main() {
-    short a,b,k=0;
+    int a, b, k = 0;
     cin >> a >> b;
-    while (a<=b) {
-        a*=3;b*=2;k++;
+ 
+    while (a <= b) {
+        a *= 3;
+        b *= 2;
+        k++;
     }
+ 
     cout << k;
     return 0;
 }
