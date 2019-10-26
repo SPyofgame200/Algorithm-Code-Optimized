699cb7f8f2738c826d9c2183b87e5bee8dea3464
Hoang Anh Vo <52689671+spyofgame@users.noreply.github.com>
2019-10-26 10:19:17 +0700

    Update 785A - Anton and Polyhedrons.cpp

--- A-Problems/785A - Anton and Polyhedrons.cpp
+++ A-Problems/785A - Anton and Polyhedrons.cpp
@@ -1,32 +1,35 @@
-#include <iostream>
- 
-using namespace std;
- 
-int main() {
-    int n,cnt=0;
-    string s;
-    cin >> n;
-    while (n--) {
-        cin >> s;
-        switch(s[0])
-        {
-            case 'T': 
-                    cnt+=4; 
-                    break;
-            case 'C':
-                    cnt+=6;
-                    break;
-            case 'O':
-                    cnt+=8;
-                    break;
-            case 'D':
-                    cnt+=12;
-                    break;
-            case 'I':
-                    cnt+=20;
-                    break;
+    #include <iostream>
+     
+    using namespace std;
+     
+    int main() {
+        int q, cnt=0;
+        cin >> q;
+        
+        while (q--) {
+            string s;
+            cin >> s;
+            
+            switch(s[0])
+            {
+                case 'T': 
+                        cnt+=4; 
+                        break;
+                case 'C':
+                        cnt+=6;
+                        break;
+                case 'O':
+                        cnt+=8;
+                        break;
+                case 'D':
+                        cnt+=12;
+                        break;
+                case 'I':
+                        cnt+=20;
+                        break;
+            }
         }
+        
+        cout << cnt;
+        return 0;
     }
-    cout << cnt;
-    return 0;
-}
