4fd16e24a161087095c454f6ba900d6ab7b5f573
Hoang Anh Vo <52689671+spyofgame@users.noreply.github.com>
2019-11-02 11:09:55 +0700

    Create  177A1 - Good Matrix Elements.cpp

---
+++ A-Problems/177A1 - Good Matrix Elements.cpp
@@ -0,0 +1,21 @@
+    #include <iostream>
+    #include <algorithm>
+     
+    using namespace std;
+     
+    int main() {
+        int n, x;
+        cin >> n;
+        
+        int sum = 0, mid = (n + 1) / 2;
+     
+        for (int i = 1; i <= n; ++i)
+            for (int j = 1; j <= n; ++j) {
+                cin >> x;
+                if ((i == mid) || (j == mid) || (abs(mid - i) == abs(mid - j)))
+                    sum += x;
+            }
+     
+        cout << sum;
+        return 0;
+    }
