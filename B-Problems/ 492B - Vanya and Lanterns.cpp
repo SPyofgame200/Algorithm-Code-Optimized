998ae362c80bc30e7ccebf97b4de75a41b86817f
Hoang Anh Vo <52689671+spyofgame@users.noreply.github.com>
2019-11-02 10:35:20 +0700

    Update and rename 492B - Vanya and Lanterns.cpp to  492B - Vanya and Lanterns.cpp

--- B-Problems/ 492B - Vanya and Lanterns.cpp
+++ B-Problems/ 492B - Vanya and Lanterns.cpp
@@ -12,12 +12,12 @@ int main() {
     }
     
     sort(pos, pos +n);
-    int rad = 2*max(pos[0],l-pos[n-1]);
+    int rad = 2 * max(pos[0], l - pos[n-1]);
     for (int i = 0; i < n-1; ++i) {
-        rad = max(rad,pos[i+1]-pos[i]);
+        rad = max(rad, pos[i+1] - pos[i]);
     }
     
     cout.precision(10);
-    cout << fixed << rad/2.0;
+    cout << fixed << rad / 2.0;
     return 0;
 }
