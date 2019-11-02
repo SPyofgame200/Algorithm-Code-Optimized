2fcc669f6ebc98b0fc71c2927a605e0830462421
Hoang Anh Vo <52689671+spyofgame@users.noreply.github.com>
2019-11-02 10:34:51 +0700

    Update and rename 266B - Queue at the School.cpp to  266B - Queue at the School.cpp

--- B-Problems/ 266B - Queue at the School.cpp
+++ B-Problems/ 266B - Queue at the School.cpp
@@ -9,12 +9,13 @@ int main() {
  
     while (q--) {
         for (int i = 1; i < s.size(); ++i) {
-            if (s[i-1]=='B' && s[i]=='G') {
+            if (s[i-1] == 'B' && s[i] == 'G') {
                 swap(s[i-1], s[i]);
                 ++i;
             }
         }
     }
+ 
     cout << s;
     return 0;
 }
