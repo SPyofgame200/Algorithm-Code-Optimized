71436b41d85db9a979f81b0f02037453bdf4fd21
Hoang Anh Vo <52689671+spyofgame@users.noreply.github.com>
2019-11-02 09:41:33 +0700

    Update and rename 041A - Translation.cpp to 0041A - Translation.cpp

--- A-Problems/0041A - Translation.cpp
+++ A-Problems/0041A - Translation.cpp
@@ -6,7 +6,7 @@ int main() {
     string s,t;
     cin >> s >> t;
     for (int i = 0; i < s.size(); ++i) {
-        if (s[i]!=t[s.size()-i-1]) {
+        if (s[i] != t[s.size()-i-1]) {
             cout << "NO";
             return 0;
         }
