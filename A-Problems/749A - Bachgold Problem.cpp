3e6cc4a0121883cf5187c50bd1ab3981f8852d32
Hoang Anh Vo <52689671+spyofgame@users.noreply.github.com>
2019-11-02 10:54:50 +0700

    Create  749A - Bachgold Problem.cpp

---
+++ A-Problems/749A - Bachgold Problem.cpp
@@ -0,0 +1,16 @@
+    #include <iostream>
+     
+    using namespace std;
+     
+    int main() {
+        int n;
+        cin >> n;
+     
+        cout << n / 2 << endl;
+        for (int i = 0; i < n / 2 - 1; ++i) {
+            cout << 2 << " ";
+        }
+        
+        cout << ((n&1) ? 3 : 2);
+        return 0;
+    }
